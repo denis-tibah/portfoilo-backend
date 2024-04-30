9af53fbcfed02329162c439a2501508d90651c78
Elliot Négrel-Jerzy <elliot@escape.tech>
2024-04-30 18:10:26 +0200

    revert: select port in cors selector

--- next.config.mjs
+++ next.config.mjs
@@ -9,7 +9,7 @@ const nextConfig = {
           { key: "Access-Control-Allow-Origin", value: "https://bsodium.fr" },
           {
             key: "Access-Control-Allow-Origin",
-            value: "*://localhost:*/*",
+            value: "http://localhost:3000",
           },
           {
             key: "Access-Control-Allow-Methods",
