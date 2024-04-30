520363691abf3c8c0e9ee2c90202e6731b746095
Elliot Négrel-Jerzy <elliot@escape.tech>
2024-04-30 17:53:27 +0200

    feat: add localhost to cors whitelist

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,10 @@ const nextConfig = {
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "https://bsodium.fr" },
           {
+            key: "Access-Control-Allow-Origin",
+            value: "http://localhost:3000",
+          },
+          {
             key: "Access-Control-Allow-Methods",
             value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
           },
