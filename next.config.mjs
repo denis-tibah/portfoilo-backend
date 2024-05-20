400964bf867cd87960fb6db8f122c11e4d1df736
Elliot Négrel-Jerzy <negreljerzy.elliot@gmail.com>
2024-05-20 17:01:43 +0200

    chore: Update next js middleware and package.json dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -1,33 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "https://bsodium.fr" },
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "https://www.bsodium.fr",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-          {
-            key: "Cache-Control",
-            value: "public, s-maxage=1800, stale-while-revalidate=60",
-          },
-        ],
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
