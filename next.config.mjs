899bb5771d8a5d8e8037a4bc9714fee4b417671c
Elliot Négrel-Jerzy <negreljerzy.elliot@gmail.com>
2024-05-20 14:35:33 +0200

    chore: Update Access-Control-Allow-Origin value in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,11 @@ const nextConfig = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Origin", value: "https://bsodium.fr" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://www.bsodium.fr",
+          },
           {
             key: "Access-Control-Allow-Methods",
             value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
